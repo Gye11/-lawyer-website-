2d81b154172ea4202591d8ad05e563564ddaa303
Gye11 <hilallornekk@gmail.com>
2026-08-01 16:22:13 +0300

    final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-setTimeout(() => {
-  const intro = document.getElementById("intro");
+document.addEventListener("DOMContentLoaded", () => {
+  const intro = document.querySelector(".intro");
 
-  intro.style.transition = "opacity 1s ease";
-  intro.style.opacity = "0";
-
+  // 4 saniye sonra intro kaybolsun
   setTimeout(() => {
-    intro.style.display = "none";
-    document.body.style.overflow = "auto";
-  }, 1000);
-}, 3000);
+    intro.style.transition = "opacity 1s ease";
+    intro.style.opacity = "0";
+
+    setTimeout(() => {
+      intro.style.display = "none";
+      document.body.style.overflow = "auto";
+    }, 1000);
+  }, 4000);
+});
