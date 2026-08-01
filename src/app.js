97e8556b32ba3b1e7583ac8a1700a364d90054a1
Gye11 <hilallornekk@gmail.com>
2026-08-01 16:32:48 +0300

    final

--- src/app.js
+++ src/app.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const intro = document.querySelector(".intro");
-
-  // 4 saniye sonra intro kaybolsun
-  setTimeout(() => {
-    intro.style.transition = "opacity 1s ease";
-    intro.style.opacity = "0";
-
-    setTimeout(() => {
-      intro.style.display = "none";
-      document.body.style.overflow = "auto";
-    }, 1000);
-  }, 4000);
-});
+setTimeout(() => {
+  document.querySelector(".intro").style.opacity = "0";
+}, 3500);
